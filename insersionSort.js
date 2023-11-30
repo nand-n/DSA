85d68e13989f6084a449e981c58f181b7be2459e
Nahom Debele <nahomdebele002@gmail.com>
2023-11-30 17:06:57 +0300

    added insersion sort

---
+++ insersionSort.js
@@ -0,0 +1,16 @@
+function insersionSort(arr){
+  for(i=1; arr.length; i++){
+    let numberToInsert = arr[i]
+    let j = i-1
+    while(j>=0 && arr[j]> numberToInsert){
+        arr[j+1] = arr[j]
+        j=j-1
+    }
+  arr[j+1] = numberToInsert
+
+  }
+}
+
+let arr =[20,22,5,-5 ,-3 ,2]
+insersionSort(arr) 
+console.log(arr);

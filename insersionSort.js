62bbfedceba5255700836bb2005c01300cafeea5
Nahom Debele <nahomdebele002@gmail.com>
2023-12-01 20:15:42 +0300

    added a quck Sort

--- insersionSort.js
+++ insersionSort.js
@@ -1,13 +1,12 @@
 function insersionSort(arr){
-  for(i=1; arr.length; i++){
+  for(i=1; i<arr.length; i++){
     let numberToInsert = arr[i]
     let j = i-1
     while(j>=0 && arr[j]> numberToInsert){
-        arr[j+1] = arr[j]
-        j=j-1
+      arr[j+1] = arr[j]
+      j=j-1
     }
-  arr[j+1] = numberToInsert
-
+    arr[j+1] = numberToInsert
   }
 }
 

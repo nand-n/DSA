366ab49b06d8774e1dfcb0b6cc1bc8cc35814c75
Nahom Debele <nahomdebele002@gmail.com>
2024-01-03 15:23:06 +0300

    fixed the matrix

--- Ch5/multiDimentionalArray.js
+++ Ch5/multiDimentionalArray.js
@@ -19,30 +19,34 @@ function spiralPrint(M){
         leftCol=0,
         btmRow =M.length -1,
         rightCol = M[0].length -1;
-    
-    while (topRow < btmRow && leftCol <rightCol){
-        for(let col =0; col<=rightCol; col++){
+    while(topRow < btmRow && leftCol < rightCol){
+        for(let col=0; col<=rightCol; col++){
             console.log(M[topRow][col]);
         }
         topRow++
-        for(let row=topRow;row<=btmRow; row++){
+        for(let row=topRow; row<=btmRow; row++){
             console.log(M[row][rightCol]);
         }
         rightCol--
-        if(topRow <= btmRow){
-            for(let col = rightCol; col>=0; col--){
+        if(topRow<= btmRow){
+            for(let col=rightCol; col>=0; col--){
                 console.log(M[btmRow][col]);
             }
-            btmRow--
         }
-        if(leftCol <=rightCol){
+        btmRow--
+        if(leftCol<=rightCol){
             for(let row = btmRow; row>topRow; row--){
                 console.log(M[row][leftCol]);
             }
-            leftCol--
+            leftCol++
         }
     }
-
+    
 }
 
-console.log(spiralPrint([['O','-','X'], ['-','O','-'], ['-','X','O']]));
+console.log(spiralPrint([
+            [1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10],
+            [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20]
+        ]));

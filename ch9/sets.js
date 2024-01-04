b2e36944b8c4372e0ad05c356db4b7f13aa66e44
Nahom Debele <nahomdebele002@gmail.com>
2024-01-04 17:11:58 +0300

    added set

---
+++ ch9/sets.js
@@ -0,0 +1,40 @@
+const set = new Set()
+set.add(1)
+set.add(2)
+set.add(3)
+set.add(4)
+set.add(5)
+
+// console.log(set);
+
+//Intersection
+
+function intersectSet(setA , setB){
+    let intersection = new Set()
+    for(var elem of setB){
+        if(setA.has(elem)){
+            intersection.add(elem)
+        }
+    }
+    return intersection
+}
+
+var setA = new Set([1, 2, 3, 4]),setB = new Set([2, 3]);
+console.log("Intersections :",intersectSet(setA,setB));
+
+
+
+//Superset : a set is a "superset " of another set if it contains all the elements of the other set
+function isSuperSet(setA , subSet){
+    for(let elem of subSet){
+        if(!setA.has(elem)){
+            return false
+        }
+    }
+    return false
+}
+
+var setA = new Set([1, 2, 3, 4]),setB = new Set([2, 3]),setC = new Set([5]);
+
+console.log("Is super set : ",isSuperSet(setA , setB));
+

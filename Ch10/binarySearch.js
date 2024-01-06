41c9864ea087a32609ebb7322ec3585b64ea0a46
Nahom Debele <nahomdebele002@gmail.com>
2024-01-06 22:31:20 +0300

    added searching algorithms

--- Ch10/binarySearch.js
+++ Ch10/binarySearch.js
@@ -1,7 +1,7 @@
 function binarySearch(arr,n){
-   let lowIndex = 0 , highIndex = arr.length -1;
-   while(lowIndex<=highIndex){
-    let midIndex = Math.floor((lowIndex + highIndex) /2)
+   let lowIndex = 0, highIndex = arr.length-1;
+   while (lowIndex<=highIndex){
+    let midIndex = Math.floor((lowIndex +highIndex)/2);
     if(arr[midIndex] == n){
         return true
     }else if(n>arr[midIndex]){
@@ -10,7 +10,6 @@ function binarySearch(arr,n){
         highIndex = midIndex
     }
    }
-   return false
 }
 
 console.log(binarySearch([1,2,3,4], 4));

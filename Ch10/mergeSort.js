eb8a63d1cc8329f198e722bde75cd0c20a1b8be1
Nahom Debele <nahomdebele002@gmail.com>
2024-01-07 21:23:53 +0300

    added the merge sort

--- Ch10/mergeSort.js
+++ Ch10/mergeSort.js
@@ -12,3 +12,14 @@ function merge(leftA , rightA){
     let leftRemains = leftA.slice(leftIndex),rightRemains =rightA.slice(rightIndex)
     return results.concat(leftRemains).concat(rightRemains)
 }
+
+function mergeSort(arr){
+    if(arr.length<2){
+        return arr
+    }
+    let midPoint = Math.floor((arr.length)/2),
+        leftArray = arr.slice(0,midPoint),
+        rightArray = arr.slice(midPoint);
+        return merge(mergeSort(leftArray) , mergeSort(rightArray))
+
+}

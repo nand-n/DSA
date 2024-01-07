86e106dbeb5450d24f172d6f7c1912773c9fb5f3
Nahom Debele <nahomdebele002@gmail.com>
2024-01-07 22:47:08 +0300

    added a hash table algorithm

---
+++ Ch11/hashTable.js
@@ -0,0 +1,7 @@
+//Fixed sized data structure in which the size is defined at the start 
+{/**
+ Hash table is exelent for qucik storage and retricval of data based on
+ key - kalue parirs. 
+ In js Objects works on defining a key (propery) and its associated value
+*/}
+

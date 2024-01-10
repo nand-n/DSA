3421289ade8b0368b069da8664df29eb1bda1bf2
Nahom Debele <nahomdebele002@gmail.com>
2024-01-10 14:29:49 +0300

    added the Doubly Linked list insertion at front

--- Ch13/doublyLinkedList.js
+++ Ch13/doublyLinkedList.js
@@ -13,3 +13,22 @@ function DoublyLinkedList(){
 DoublyLinkedList.prototype.isEmpty = function(){
     return this.size == 0
 }
+
+
+DoublyLinkedList.prototype.addATFront = function(value){
+    if(this.head == null){
+        this.head = new DoublyLinkedListNode(value)
+        this.tail = this.head
+    }else{
+        let temp = new DoublyLinkedListNode(value)
+        temp.next = this.head
+        this.head.prev = temp
+        this.head = temp
+    }
+    this.size++
+}
+
+let dll1 = new DoublyLinkedList();
+dll1.insertAtHead(10); // ddl1's structure: tail: 10  head: 10
+dll1.insertAtHead(12); // ddl1's structure: tail: 10  head: 12
+dll1.insertAtHead(20); // ddl1's structure: tail: 10  head: 20

5f09d33b2cf6672adfb0a3742e4d5de4c61579cb
Nahom Debele <nahomdebele002@gmail.com>
2024-01-10 15:27:28 +0300

    added the caching algorithm

---
+++ Ch14/lfu.js
@@ -0,0 +1,25 @@
+function LFUNode(key , value){
+    this.prev = null 
+    this.next = null 
+    this.key = key
+    this.data = value
+    this.freqCount = 1
+}
+
+function LFUDoublyLinkedList(){
+    this.head = new LFUNode('buffer head' , null)
+    this.tail = new LFUNode('buffer tail' , null)
+    this.head.next = this.tail
+    this.tail.prev = this.head
+    this.size = 0
+
+}
+
+function LFUCache(capacity){
+    this.key = {}//stores LFUNode
+    this.freq ={} // stores LFUDoublyLinkedList
+    this.capacity= capacity
+    this.minFreq=0
+    this.size = 0
+}
+

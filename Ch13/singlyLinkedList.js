8d0e8540d9e8e28426fd2919b05b079f4bd57629
Nahom Debele <nahomdebele002@gmail.com>
2024-01-10 14:11:56 +0300

    added the  search on Lingly Linkedlis

--- Ch13/singlyLinkedList.js
+++ Ch13/singlyLinkedList.js
@@ -34,3 +34,50 @@ SinglyLinkedList.prototype.insert = function(value){
 let sll1 = new SinglyLinkedList()
 sll1.insert(1) // linked list is now : 1 -> null
 sll1.insert(2) // linked list is now : 2 -> 1 -> null
+
+
+//Deletion by value 
+SinglyLinkedList.prototype.remove = function(value){
+    let currentHead = this.head
+    if(currentHead.data ==value){
+        //just shift the head over . head is now this new 
+        this.head = currentHead.next
+        this.size--;
+    }else{
+        let prev = currentHead;
+        while(currentHead.next){
+            if(currentHead.data == value){
+                //remove by skipping
+                prev.next = currentHead.next
+                prev = currentHead
+                currentHead= currentHead.next
+                break //break out of the loop 
+            }
+            prev = currentHead
+            currentHead = currentHead.next
+        }
+        //if wasn't in the middle or head must be tail 
+        if(currentHead.data == value){
+            prev.next = null
+        }
+        this.size--
+    }
+}
+
+sll1.insert(1)
+sll1.insert(12)
+sll1.insert(20); // linked list is now: 20 -> 12 -> 1 -> null
+sll1.remove(12); // linked list is now: 20 -> 1 -> null
+sll1.remove(20); // linked list is now: 1 -> null
+
+//Search 
+SinglyLinkedList.prototype.find = function (value){
+    let currentHead = this.head;
+    while(currentHead.next){
+        if(currentHead.data ==value){
+            return true
+        }
+        currentHead = currentHead.next
+    }
+    return false
+}

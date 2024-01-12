3a7ae1b9561af7788dcd5f94ddf5ab093fa2b2fa
Nahom Debele <nahomdebele002@gmail.com>
2024-01-12 22:15:53 +0300

    added the binary search tree insertion

---
+++ Ch15/binarySearchTree.js
@@ -0,0 +1,36 @@
+function BinarySearchTree(){
+    this._root = null
+}
+BinarySearchTree.prototype.insert = function(value){
+    let thisNode = {left:null , right:null , value:value}
+    if(!this._root){
+        //if there is no root value yet 
+        this._root = thisNode
+    }else{
+        //loop traverse unitl 
+        let currentRoot = this._root
+        while(true){
+            if(currentRoot.value>value){
+                //let's ilncrement if it's not a null and insert if it is a null
+                if(currentRoot.left != null){
+                    currentRoot = currentRoot.left
+                }else{
+                    currentRoot.left = thisNode
+                    break
+                }
+            }else if(currentRoot.value<value){
+                //if bigger than current , put it on the right 
+                //let's in crement if it's not a nul and insert if it is a null
+                if(currentRoot.right != null){
+                    currentRoot= currentRoot.right
+                }else{
+                    currentRoot.right=thisNode
+                    break
+                }
+            }else{
+                //case that both are the same 
+                break
+            }
+        }
+    }
+}

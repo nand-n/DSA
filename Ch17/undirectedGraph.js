47b42974c8896f26861223e95867b97e2a060929
Nahom Debele <nahomdebele002@gmail.com>
2024-01-15 23:29:25 +0300

    added the undirected graph

--- Ch17/undirectedGraph.js
+++ Ch17/undirectedGraph.js
@@ -8,4 +8,10 @@ function UndirectedGraph(){
 UndirectedGraph.prototyp.addVertex = function (vertex){
     this.edges[vertex] = {}
 }
-
+UndirectedGraph.prototyp.addEdge =function(vertex1 , vertex2 , weight){
+    if(weight ==undefined){
+        weight = 0
+    }
+    this.edges[vertex1][vertex2] = weight
+    this.edges[vertex2][vertex1] = weight
+}

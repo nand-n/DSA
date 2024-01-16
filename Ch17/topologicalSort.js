068a8937160fd9ec3ea44ac70ad5d1804ba05fb9
Nahom Debele <nahomdebele002@gmail.com>
2024-01-16 22:11:57 +0300

    added the topologicalSorting algorithm

--- Ch17/topologicalSort.js
+++ Ch17/topologicalSort.js
@@ -17,3 +17,12 @@ DirectedGraph.prototype.topologicalSortUtil = function(v, visited , stack){
     }
     stack.unshift(v)
 }
+DirectedGraph.prototype.topologicalSort = function(){
+    let visited = {} , stack =[]
+    for(let item in this.edges){
+        if(visited.has(item) ==false){
+            this.topologicalSortUtil(item , visited, stack)
+        }
+    }
+    return stack
+}

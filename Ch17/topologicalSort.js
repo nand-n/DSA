59780101481799d0cc30d823c20055b8826d8c43
Nahom Debele <nahomdebele002@gmail.com>
2024-01-16 22:09:44 +0300

    added the topologicalSorting algorithm

---
+++ Ch17/topologicalSort.js
@@ -0,0 +1,19 @@
+import DirectedGraph from "./directedGraph";
+
+{/**
+    Topological sorting has a visited set to ensure that the recurive call does not result 
+    in an infinite loop. 
+    For a given node , that node is added to the visited set, and its  neighbours that have 
+    not been visited are visited in the next recurive call. 
+    At the end of the recursive call , unshifted is used 
+*/}
+
+DirectedGraph.prototype.topologicalSortUtil = function(v, visited , stack){
+    visited.add(v)
+    for(let item in this.edges[v]){
+        if(visited.has(item) ==false){
+            this.topologicalSortUtil(item , visited , stack)
+        }
+    }
+    stack.unshift(v)
+}

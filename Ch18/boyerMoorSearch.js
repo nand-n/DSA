16359fd8b73808c77b74b3ffc45cdf9f16bf87ed
Nahom Debele <nahomdebele002@gmail.com>
2024-01-17 20:03:55 +0300

    fixed boyer moor search algorithm

--- Ch18/boyerMoorSearch.js
+++ Ch18/boyerMoorSearch.js
@@ -11,18 +11,28 @@
     roi{r: 2, o: 1, i: 3}
 */}
 
-function buildBadMatchTable(str){
-    let tableObj = {} , strLength = str.length;
-    for(let i = 0; i<strLength -1; i++){
-        tableObj[str[i]] = strLength -1 -i;
-    } 
-    if(tableObj[str[strLength-1] == undefined]){
-        tableObj[str[strLength -1]] == strLength
+// function buildBadMatchTable(str){
+//     var tableObj = {} , strLength = str.length;
+//     for(let i = 0; i<strLength -1; i++){
+//         tableObj[str[i]] = strLength -1 -i;
+//     } 
+//     if(tableObj[str[strLength-1] == undefined]){
+//         tableObj[str[strLength -1]] == strLength
+//     }
+//     return tableObj
+// }
+
+function buildBadMatchTable(str) {
+        var tableObj = {},
+            strLength = str.length;
+        for (var i = 0; i <  strLength - 1; i++) {
+            tableObj[str[i]] = strLength - 1 - i;
+        }
+        if (tableObj[str[strLength-1]] == undefined) {
+            tableObj[str[strLength-1]] = strLength;
+        }
+        return tableObj;
     }
-    return tableObj
-}
-
-
 function boyerMoor(str , pattern){
     let badMatchTable = buildBadMatchTable(pattern) , offSet =0, patternLastIndex = pattern.length -1,scanIndex = patternLastIndex , maxOffset =str.length -pattern.length;
     //if the offset is bigger than maxOffSet , cannot be found 
@@ -51,3 +61,5 @@ buildBadMatchTable('data');// {d: 3, a: 2, t: 1}
 buildBadMatchTable('struct');// {s: 5, t: 4, r: 3, u: 2, c: 1}
 buildBadMatchTable('roi');// {r: 2, o: 1, i: 3}
 console.log(buildBadMatchTable('jam'));// {j: 2, a: 1, m: 3}
+
+console.log(boyerMoor('jellyjam','jelly'));

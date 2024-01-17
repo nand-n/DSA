d98fd7735466e21c80bacda073e4d5f2e4dc2a0e
Nahom Debele <nahomdebele002@gmail.com>
2024-01-17 11:31:14 +0300

    added tire node search algorithm

--- Ch18/tireNode.js
+++ Ch18/tireNode.js
@@ -36,8 +36,29 @@ Tire.prototype.insert = function(word){
 }
 
 
+{/**
+    To search inside a tire , each characted of the word must be checked. 
+    This is done by setting  a temporary variable of current on the root
+    The currenct variable is updated as each characted in the word is checked 
+*/}
+
+Tire.prototype.search = function(word){
+    let current = this.root
+    for(let i = 0; i<word.length; i++){
+        let ch = word.charAt(i)
+        let node = current.children[ch]
+        if(node == null){
+            return false //node doesnt exist
+        }
+        current = node 
+    }
+    return current.endOfWord
+}
+
 var trie1 = new Tire();
 trie1.insert("sammie");
 trie1.insert("simran");
-console.log(trie1);
-// trie1.search("simran"); // true
+trie1.insert("nana");
+
+console.log(trie1.search("simran")); // true
+// console.log(trie1);

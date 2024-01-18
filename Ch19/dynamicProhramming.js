2bbe5794d5a4e3a25d7182e586a95f733a16988f
Nahom Debele <nahomdebele002@gmail.com>
2024-01-18 22:12:57 +0300

    added the dynamic programming example fibbonacci sequence

--- Ch19/dynamicProhramming.js
+++ Ch19/dynamicProhramming.js
@@ -7,3 +7,11 @@ function getNthFibo(n){
     }
 
 }
+
+
+{/** 
+    Recursive implementation of this algorithm is O(2^n)
+    This is an nexponential runtime , which is impractical for 
+    real-worled applications. 
+    
+*/}

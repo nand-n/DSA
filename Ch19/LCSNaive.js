9df7e5f34eb963516e836c2bafa2ee95c39703b7
Nahom Debele <nahomdebele002@gmail.com>
2024-01-20 18:24:07 +0300

    added the LCSNaive algorithm DP

--- Ch19/LCSNaive.js
+++ Ch19/LCSNaive.js
@@ -38,3 +38,21 @@ console.log(LCSNaiveWrapper('AGGTAB', 'GXTXAYB')); // 4
     Each item in a matrix at a row , i , and column , j , represents LCS(str1[0:i] , str2[0:j])
     
 */}
+function longestCommonSequenceLength(str1 , str2){
+    let matrix = Array(str1.length +1).fill(Array(str2.length +1).fill(0)),
+    rowLength = str1.length +1,
+    colLength = str2.length +1,
+    max = 0;
+
+    for(let row = 1; row<rowLength; row++){
+        for(let col = 1; col<colLength; col++){
+            let str1Char = str1.charAt(row -1),
+                str2Char = str2.charAt(col - 1);
+                if(str1Char == str2Char){
+                    matrix[row][col]  = matrix[row-1][col-1] +1;
+                    max = Math.max(matrix[row][col] , max) 
+                }
+        }
+    }
+    return max;
+}
